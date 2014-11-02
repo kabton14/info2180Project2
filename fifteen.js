c0d94607454eef3e9a7ae31adb409ea3ea214fa4
Kevin <kabton14@gmail.com>
2014-11-01 22:00:27 -0500

    update of fifteen.js

--- fifteen.js
+++ fifteen.js
@@ -1,3 +1,13 @@
+/*INFO 2180 Project 2
+  ID# 620040594
+
+  +++++++++++++++++
+  ADDITIONAL FEATURES IMPLEMENTED:
+  -Custom background
+
+*/
+
+
 "use strict";
 
 var tiles; // Variable to store tile divs.
@@ -6,17 +16,31 @@ var moveable; //Variable to store whether clicked tile is movable or not
 
 window.onload = function(){
 
-tiles = $("puzzlearea").getElementsByTagName("div"); //getting list of tiles (divs).
+  tiles = $("puzzlearea").getElementsByTagName("div"); //getting list of tiles (divs).
 
-start();
+  start();
 
-$("shufflebutton").onclick = shuffle;
+  $("shufflebutton").onclick = shuffle;
+
+  selectView();
+
+  $("change_btn").onclick = setBackground;
 
 };
 
 /*The below function loads the tiles, tile image, and sets the event listeners*/
 
 function start() {
+
+  var a = "url('http://i187.photobucket.com/albums/x37/kabton14/info2180%20project/smurf_zpsd17fd548.jpg')";
+  var b = "url('http://i187.photobucket.com/albums/x37/kabton14/info2180%20project/quarry_zpsc410258d.jpg')";
+  var c = "url('http://i187.photobucket.com/albums/x37/kabton14/info2180%20project/Crimson-Staff_avatar_zps061aa7fc.png')";
+  var d = "url('http://i187.photobucket.com/albums/x37/kabton14/info2180%20project/anthony-burrill-owl_zps3863e809.png')";
+  var e = "url('background.jpg')";
+  var background = [a,b,c,d,e];
+  var rand_background = background[Math.floor(Math.random() * (background.length - 1))];
+
+
 
     var top = 0, left = 0; // position variables of each tile.
     var b_y = 0, b_l = 0; //position of the background image
@@ -27,6 +51,7 @@ function start() {
 
         tiles[i].style.left = left + "px"; //adding left position to current tile.
         tiles[i].style.top =  top + "px"; //adding top position to current tile.
+        tiles[i].style.backgroundImage = rand_background;
 
         tiles[i].style.backgroundPosition =  b_l + "px " + b_y + "px";
 
@@ -49,13 +74,18 @@ function start() {
         tiles[i].onmouseover = canBeMoved;
         tiles[i].onclick = moveTile;
 
+
+
     }
+
+
+
 
 }
 
 /* Function to move tile that is beside empty space*/
 
-function moveTile(tile){
+function moveTile(){
 
     var top_pos = parseInt(this.getStyle("top")); //getting current position of tile
     var left_pos = parseInt(this.getStyle("left"));
@@ -127,37 +157,10 @@ function findTile(top,left){
   }
 }
 
+
+
 /* Function to shuffle tiles*/
 function shuffle(){
-  var neighbours = [];
-  for (var i = 0; i < 1000; i++){
-    for(var x=0; x < tiles.length; x++){
-      if(tiles[x].canBeMoved()){
-        neighbours.push(tiles[x]);
-      }
-    }
-  }
-    //randomly choose an element i from neighbors.
-    var chosen_one = neighbours[Math.floor(Math.random() * (neighbours.length - 1))];
-
-
-    //move neighbors[i] to the location of the empty square.
-    if(neighbours !== null){
-    var top = chosen_one.getStyle("top");
-    var left = chosen_one.getStyle("left");
-
-    chosen_one.style.top = empty_tile.y;
-    chosen_one.style.left = empty_tile.x;
-
-    empty_tile.x = left;
-    empty_tile.y = top;
-
-  }
-
-}
-
-
-/*
 
   var main = document.getElementById("puzzlearea");
   var divs = main.children;
@@ -169,6 +172,82 @@ function shuffle(){
     main.appendChild(frag);
 
 
-}*/
+  start();
 
-/* Function to change background*/
+}
+
+
+
+
+
+
+
+
+
+/* Function insert area to change background*/
+function selectView(){
+
+  var x = document.getElementById("overall");
+  var select = document.createElement("select");
+  select.id = "b_select";
+  var option1 = document.createElement("option");
+  var option2 = document.createElement("option");
+  var option3 = document.createElement("option");
+  var option4 = document.createElement("option");
+
+  option1.text= "Background1";
+  option1.id = "option1";
+  option2.text= "Background2";
+  option2.id = "option2";
+  option3.text= "Background3";
+  option3.id = "option3";
+  option4.text= "Background4";
+  option4.id = "option4";
+
+  select.appendChild(option1);
+  select.appendChild(option2);
+  select.appendChild(option3);
+  select.appendChild(option4);
+
+  x.appendChild(select);
+
+  var btn = document.createElement("button");
+  btn.id = "change_btn";
+  var t = document.createTextNode("Change Background");
+  btn.appendChild(t);
+  x.appendChild(btn);
+
+
+
+
+}
+
+/* Function to set background*/
+function setBackground(){
+
+  var background = $("b_select").value;
+
+  switch(background){
+
+    case "Background1":
+      for(var x = 0; x < tiles.length; x++)tiles[x].style.backgroundImage = "url('http://i187.photobucket.com/albums/x37/kabton14/info2180%20project/smurf_zpsd17fd548.jpg')";
+      break;
+
+    case "Background2":
+      for(var x = 0; x < tiles.length; x++)tiles[x].style.backgroundImage = "url('http://i187.photobucket.com/albums/x37/kabton14/info2180%20project/quarry_zpsc410258d.jpg')";
+      break;
+
+    case "Background3":
+      for(var x = 0; x < tiles.length; x++)tiles[x].style.backgroundImage = "url('http://i187.photobucket.com/albums/x37/kabton14/info2180%20project/Crimson-Staff_avatar_zps061aa7fc.png')";
+      break;
+
+    case "Background4":
+      for(var x = 0; x < tiles.length; x++)tiles[x].style.backgroundImage = "url('http://i187.photobucket.com/albums/x37/kabton14/info2180%20project/anthony-burrill-owl_zps3863e809.png')";
+      break;
+
+    default:
+      for(var x = 0; x < tiles.length; x++)tiles[x].style.backgroundImage = "url('background.jpg')";
+
+  }
+
+}
